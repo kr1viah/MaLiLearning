e70bca55ca92330eeed73c2abbf82ee75b416c6c
kr1viah_ <kravpolet@gmail.com>
2025-08-06 18:03:11 +0200

    Initial commit

---
+++ src/client/java/org/kr1v/malilearning/client/MalilearningClient.java
@@ -0,0 +1,10 @@
+package org.kr1v.malilearning.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MalilearningClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/main/java/org/kr1v/malilearning/Malilearning.java
@@ -0,0 +1,10 @@
+package org.kr1v.malilearning;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Malilearning implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
